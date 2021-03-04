dcc9daa57efe61492799add39e2086333d85415b
j-000 <jjasilva85@gmail.com>
2021-03-04 23:04:47 +0000

    Upgrade

--- config.py
+++ config.py
@@ -21,5 +21,5 @@ class DevelopmentConfig(Config):
     DEBUG = True
     ENV = 'development'
     SERVER_NAME = '127.0.0.1:5000'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///devdata.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
 
